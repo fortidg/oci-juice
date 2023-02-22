0688ceed1d7e04383f05f945f9a7d48a800354e3
fortidg <dgooch@fortinet.com>
2023-02-22 16:39:18 +0000

    updating the boot.sh file

--- boot.sh
+++ boot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 echo "Wait for Internet access"
 while ! curl --connect-timeout 3 "http://www.google.com" &> /dev/null
     do continue
 done
+sleep 5
 apt-get update -y
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker pull bkimminich/juice-shop
